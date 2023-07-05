595da3065335764eee664aea57451000b985ecef
ashly <ashlyannarackal@gmail.com>
2023-07-05 09:01:44 +0530

    permissions added

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:permission_handler/permission_handler.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -17,7 +18,6 @@ class MyApp extends StatefulWidget {
   @override
   State<MyApp> createState() => _MyAppState();
 
-  // ignore: library_private_types_in_public_api
   static _MyAppState? of(BuildContext context) {
     return context.findAncestorStateOfType<_MyAppState>();
   }
@@ -33,8 +33,44 @@ class _MyAppState extends State<MyApp> {
   }
 
   @override
+  void initState() {
+    super.initState();
+    checkAndRequestPermissions();
+  }
+
+  Future<void> checkAndRequestPermissions() async {
+    final status = await Permission.sms.request();
+    if (status.isGranted) {
+      // Permission granted, proceed with your app
+      Navigator.push(
+        context,
+        MaterialPageRoute(builder: (context) => const Frame()),
+      );
+    } else if (status.isDenied) {
+      // Permission denied, handle accordingly
+      showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: const Text('Permission Denied'),
+            content: const Text('Please grant SMS permission to use the app.'),
+            actions: <Widget>[
+              TextButton(
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+                child: const Text('OK'),
+              ),
+            ],
+          );
+        },
+      );
+    }
+  }
+
+  @override
   Widget build(BuildContext context) {
-    //Set the fit size (Find your UI design, look at the dimensions of the device screen and fill it in,unit in dp)
+    // Set the fit size (Find your UI design, look at the dimensions of the device screen and fill it in, unit in dp)
     return ScreenUtilInit(
       designSize: const Size(360, 640),
       minTextAdapt: true,
